89450b511591dc3458ad37d8dc5154e296731ebd
ipshitab <45273272+ipshitab@users.noreply.github.com>
2018-11-22 16:02:07 -0500

    Add files via upload

---
+++ StudentInfoCard.java
@@ -0,0 +1,58 @@
+/**
+ * This program prints a description of my personal information and my interests.
+ *
+ * @author (Ipshita Bhatnagar)
+ * @version (August 13, 2018)
+ */
+public class StudentInfoCard
+{
+public static void main(String[ ] args)
+{
+System.out.print("************************************************************************************************************************");
+System.out.println();
+System.out.print("*                                                                                                                      *");
+System.out.println();
+System.out.print("*                                                                                                                      *");
+System.out.println();
+System.out.print("*                                                                                                                      *");
+System.out.println();
+System.out.print("*                                             STUDENT INFORMATION                                                      *");
+System.out.println();
+System.out.print("*                                                                                                                      *");
+System.out.println();
+System.out.print("*                                                                                                                      *");
+System.out.println();
+System.out.print("*                                                                                                                      *");
+System.out.println();
+System.out.print("************************************************************************************************************************");
+System.out.println();
+System.out.println();
+System.out.println();
+System.out.print("Name: Ipshita Bhatnagar   Birthday: July 8   Age: 17 years old");
+System.out.println();
+System.out.print("School: Damascus High School   Grade: 12th grade   City: Clarksburg, MD");
+System.out.println();
+System.out.println();
+System.out.print("Home Phone: 301-515-5877 Cell phone: 240-409-8740 (best number to call or text) Email: ipshita.mishi@yahoo.com");
+System.out.println();
+System.out.print("I am usually avaliable during the day.");
+System.out.println();
+System.out.println();
+System.out.print("Parent contact information: Anurag Bhatnagar (Dad): 240-888-5666");
+System.out.println();
+System.out.println();
+System.out.print("This upcoming year I will take Calculus with Applications and AP Statistics.");
+System.out.println();
+System.out.println();
+System.out.print("I have taken AP Computer Science and Foundations of Technology at my school where I learned to code");
+System.out.println();
+System.out.print("and make projects. I have also participated in Forensics competitions, started my own WIT club at my school,");
+System.out.println();
+System.out.print("volunteered at many coding camps like Kids Museum, and had an internship at VariQ which is a company based on Cybersecurity.");
+System.out.println();
+System.out.println();
+System.out.print("I am taking this course because I enjoy learning new things and I believe this will help me in my future.");
+System.out.println();
+System.out.print("Also, I want to pursue Computer Science for my future career.");
+}
+}
